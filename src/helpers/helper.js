dbc8d93f7d858a53a476e8862a54029886aa1841
DenysPetroff <petroffich2008@gmail.com>
2023-06-30 20:13:32 +0300

    first

---
+++ src/helpers/helper.js
@@ -0,0 +1,16 @@
+ function add(a, b) { 
+    return a + b;
+}
+const value = 10;
+ 
+class User { 
+    constructor(name) { 
+        this.name = name;
+    }
+}
+
+export {
+    add,
+    value,
+    User
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { add, User } from "./helpers/helper";
+console.log(add);
+
+const newUser = new User('Test name');
